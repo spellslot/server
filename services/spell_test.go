3df0028eb7ca12a85c622c869ad400a6a35c4d83
Jacob Hackett <jacobrhackett@gmail.com>
2018-03-31 21:42:38 -0400

    adding services tests and main to gitignore

--- services/spell_test.go
+++ services/spell_test.go
@@ -1,1 +1,17 @@
 package services
+
+import (
+	"net/url"
+	"testing"
+
+	"github.com/spellslot/server/util"
+
+	"github.com/spellslot/server/daos"
+)
+
+func TestSimpleGet(t *testing.T) {
+	dao, _ := daos.NewSpellDAO(daos.MockDB)
+	svc := NewSpellService(dao)
+	res, _ := svc.Get(url.Values{})
+	util.AssertEqual(t, len(*res), 0)
+}
